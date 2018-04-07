e536ed2022f6437edf0fdef87c99ac52d18b3f48
NiKole Maxwell <nvymaxwell@outlook.com>
2018-04-07 04:01:12 -0400

    A | XML Request Scripts

--- src/main/script.js
+++ src/main/script.js
@@ -1,1 +1,20 @@
-console.log("Working as intended")
+//The module: file.js
+
+let getFonts = new XMLHttpRequest();
+getFonts.open("GET", '/src/main/fonts.json');
+getFonts.onload = function() {
+    console.log(getFonts.responseText);
+};
+
+getFonts.send();
+
+
+ function init() {
+    loadJSON(function(response) { 
+     // Parse JSON string into object
+       var actual_JSON = JSON.parse(response);
+    });
+   }
+
+
+   
